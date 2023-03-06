f252713f15cf8f1f46d211e608003b0f98fa1ac7
Diegoj <erasodf@gmail.com>
2023-03-06 16:36:31 -0500

    Etapas completadas: simulacion, exploracion y visualization

---
+++ BreastCancerDetector_simulation/2-data-exploration/exploration.py
@@ -0,0 +1,87 @@
+import os
+from datetime import datetime, timedelta
+
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
+
+def main():
+    while True:
+        print_menu()
+        option = get_option_choice()
+        #Check what choice was entered and act accordingly
+        options = {
+            1: option1,
+            2: option2,
+            3: option3,
+            4: option4,
+            5: exit_program
+        }
+        if option in options:
+            options[option]()
+        else:
+            print('Invalid option. Please enter a number between 1 and 4.')
+
+def print_menu():
+    menu_options = {
+        1: 'Check for Null Values',
+        2: 'Check for Missing Values',
+        3: 'Check for Duplicates',
+        4: 'Check number of new pacients',
+        5: 'Exit'
+    }
+    for key in menu_options.keys():
+        print (key, '--', menu_options[key] )
+
+def option1():
+     request_table = load_rdbms_requests_table()
+     print('\nNULL VALUES:\n',request_table.isnull().sum(),'\n')
+
+def option2():
+    request_table = load_rdbms_requests_table()
+    print("\nMissing Values:\n", request_table.isna().sum(),'\n')
+
+def option3():
+    request_table = load_rdbms_requests_table()
+    print("\nDuplicated:\n", request_table.duplicated().sum(),'\n')
+
+def option4():
+    request_table =  load_rdbms_requests_table()
+    request_table['open_date'] = pd.to_datetime(request_table['open_date'], format = '%Y-%m-%d')
+    end_date = request_table['open_date'].iloc[-1]
+    #Subtract one week from the last_entry datetime object
+    start_date = end_date - timedelta(weeks=1)
+    
+    # Filter the dataframe to include rows only between the start and end dates
+    filtered_table = request_table[(request_table['open_date'] >= start_date) & (request_table['open_date'] <= end_date)]
+
+    # Count the number of rows in the filtered dataframe
+    num_cases = len(filtered_table)
+
+    print("\nNumber pacients between \n",start_date,' and ', end_date, 'is: ',num_cases,'\n')
+
+def exit_program():
+    print('Thanks message before exiting')
+    exit()
+
+def get_option_choice():
+    while True:
+        try:
+            option = int(input('Enter your choice: '))
+            return option
+        except ValueError:
+            print('Wrong input. Please enter a number ...')
+
+def load_rdbms_requests_table():
+    module_path = os.path.dirname(__file__)
+    filename = os.path.join(module_path, "../operational_rdbms/requests_table.csv")
+    if not os.path.exists(filename):
+        raise FileNotFoundError(f"File {filename} not found")
+    data = pd.read_csv(filename, sep=",")
+    return data
+
+
+if __name__ == "__main__":
+    main()
---
+++ BreastCancerDetector_simulation/3-visualization/visualization.py
@@ -0,0 +1,118 @@
+import os
+from datetime import datetime
+
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+import tkinter as tk
+
+
+# A dictionary of menu options
+menu_options = {
+    1: "Display diagnosis count - internal control",
+    2: "Display correlation heatmap",
+    3: "Exit"
+    }
+
+
+def main():
+    
+    while(True):
+        print_menu()
+        option = ''
+        try:
+            option = int(input('Enter your choice: '))
+        except:
+            print('Wrong input. Please enter a number ...')
+        #Check what choice was entered and act accordingly
+        if option == 1:
+            display_diagnosis_count()
+        elif option == 2:
+            display_correlation_heatmap()
+        elif option == 3:
+            print('Thanks message before exiting')
+            exit()
+        else:
+            print('Invalid option. Please enter a number between 1 and 4.')
+
+def print_menu():
+    for key in menu_options.keys():
+        print (key, '--', menu_options[key] )
+
+def display_diagnosis_count():
+    """Displays the count of benign and malignant diagnosis."""
+    request_table = load_rdbms_requests_table()
+    fig, ax = plt.subplots(figsize=(8, 5))
+    request_table.diagnosis.value_counts().plot(kind="bar", width=0.1, color=["lightgreen", "cornflowerblue"], legend=1, ax=ax)
+    ax.set_xlabel("(0 = Benign) (1 = Malignant)", fontsize=12)
+    ax.set_ylabel("Count", fontsize=12)
+    ax.tick_params(axis='both', which='major', labelsize=12)
+    ax.legend(["Benign"], fontsize=12)
+    plt.show()
+
+def display_correlation_heatmap():
+    """Displays the correlation heatmap."""
+    with ScreenDimensions() as sd:
+        width= sd.width
+        height = sd.height
+
+    # Load and analyze data
+    request_table = load_rdbms_requests_table()
+    correlation_table = correlation(request_table, 0.8)
+
+    # Plot heatmap if correlation table is not empty
+    if not correlation_table.empty:
+        px = 1/plt.rcParams['figure.dpi']  # Compute pixel size based on matplotlib DPI setting
+        dims = (width*px, height*px)
+        fig, ax = plt.subplots(figsize=dims)
+        sns.heatmap(correlation_table.corr(), cmap='RdYlBu')
+        plt.show()
+
+#def option3():
+
+class ScreenDimensions:
+    def __enter__(self):
+        # Initialize the object and get the screen dimensions
+        self.root = tk.Tk()  # Create a Tk object
+        self.width = self.root.winfo_screenwidth()  # Get screen width in pixels
+        self.height = self.root.winfo_screenheight()  # Get screen height in pixels
+        return self  # Return the ScreenDimensions object to the caller
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Clean up the object when the with statement is exited
+        self.root.destroy()  # Destroy the Tk object to free up resources
+        
+def load_rdbms_requests_table():
+    module_path = os.path.dirname(__file__)
+    filename = os.path.join(module_path, "../operational_rdbms/requests_table.csv")
+    if not os.path.exists(filename):
+        raise FileNotFoundError(f"File {filename} not found")
+    data = pd.read_csv(filename, sep=",")
+    return data
+
+
+def correlation(dataset, threshold):
+#Computes the correlation matrix of the dataset and removes highly correlated columns.
+    """Args:
+        dataset (pandas.DataFrame): the input dataset.
+        threshold (float): the correlation threshold to use for column removal.
+    Returns:
+        pandas.DataFrame: the dataset with highly correlated columns removed.
+    """
+    col_corr = set() # Set of all the names of deleted columns
+    corr_matrix = dataset.corr()
+    for i in range(len(corr_matrix.columns)):
+        for j in range(i):
+            if (corr_matrix.iloc[i, j] >= threshold) and (corr_matrix.columns[j] not in col_corr):
+                colname = corr_matrix.columns[i] # getting the name of column
+                col_corr.add(colname)
+                if colname in dataset.columns:
+                    del dataset[colname] # deleting the column from the dataset
+    return(dataset)
+
+
+
+if __name__ == "__main__":
+    main()
+
